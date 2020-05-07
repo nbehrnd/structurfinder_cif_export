4fd200c62cec27c08d16f87aa1a8a5a12d138b3b
Norwid Behrnd <nbehrnd@yahoo.com>
2020-05-07 14:47:05 +0200

    Readout of HM spacegroup is ID dependent

--- test_read.py
+++ test_read.py
@@ -73,12 +73,15 @@ model_unit_cell_dimensions()
 
 def model_spacegroup():
     """ Readout the Hermann-Maguin spacegroup """
-    spacegroup_HM = ""
-    c.execute('SELECT * FROM RESIDUALS')
-    data = c.fetchall()
-    for line in data:
-        spacegroup_HM = str(str(line).strip().split(', ')[3])[1:-1]
-        print("spacegroup: ", spacegroup_HM)
+    if model_number > 0:
+        for ID in range(1, model_number + 1):
+
+            spacegroup_HM = ""
+            c.execute('SELECT * FROM RESIDUALS WHERE ID={}'.format(ID))
+            data = c.fetchall()
+            for line in data:
+                spacegroup_HM = str(str(line).strip().split(', ')[3])[1:-1]
+                print("ID; spacegroup: ", ID, spacegroup_HM)
 
 
 model_spacegroup()
